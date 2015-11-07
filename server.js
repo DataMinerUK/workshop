b93333d0fbbdd0d94092e55bcd822266e8d05462
Nicola Hughes <nicolarhughes@gmail.com>
2015-11-07 14:58:12 +0000

    Testing with simple hello world page

--- server.js
+++ server.js
@@ -4,3 +4,17 @@
  */
 
 "use strict";
+
+var http = require('http');
+var fs = require('fs');
+var port = 8888;
+
+function handler(request,response) {
+  var url = request.url;
+  if (url === "/") {
+      response.writeHead(200, {"Content-type": "text/html"});
+      response.end("<h1>Hello World!</h1>");
+  }
+}
+
+http.createServer(handler).listen(port);
